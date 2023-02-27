706ac156599099c9758d0f0da0b69860e7600153
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2023-02-27 11:29:34 -0800

    style: use links

--- components/About.tsx
+++ components/About.tsx
@@ -1,3 +1,5 @@
+import { foyerLink, tripleyLink } from "@/utils/links";
+
 export const About = () => {
   return (
     <section className="rounded-lg">
@@ -7,17 +9,34 @@ export const About = () => {
         software, and building meaningful products and solutions.
       </p>
       <p className="mb-4">
-      Currently, I work as a product engineer at <a href="https://foyer.work" target="_blank"><span className="text-[#AD58FF] font-beatriceSemibold">Foyer</span></a>, helping maximize the performance of engineering teams. 
-      </p>
-      
-      <p className="mb-4">
-      I'm also building <a href="https://tripley.app" target="_blank"><span className="text-[#3290FF] font-beatriceSemibold">Tripley</span></a>, a mobile app that enables travelers to plan and manage their trips effectively.
+        Currently, I work as a product engineer at{" "}
+        <a href={foyerLink} target="_blank">
+          <span className="text-[#AD58FF] font-beatriceSemibold">Foyer</span>
+        </a>
+        , helping maximize the performance of engineering teams.
       </p>
 
       <p className="mb-4">
-      Check out my work, and my GitHub <a href="https://github.com/eternalmoon1234" target="_blank"><span className="bg-[#74B3FF] text-primary p-1 rounded-lg leading-loose">@eternalmoon1234</span></a>. Reach out to me at <span className="text-[#74B3FF] underline">gautamparanjape8@gmail.com</span> 
+        I'm also building{" "}
+        <a href={tripleyLink} target="_blank">
+          <span className="text-[#3290FF] font-beatriceSemibold">Tripley</span>
+        </a>
+        , a mobile app that enables travelers to plan and manage their trips
+        effectively.
       </p>
-      
+
+      <p className="mb-4">
+        Check out my work, and my GitHub{" "}
+        <a href="https://github.com/eternalmoon1234" target="_blank">
+          <span className="bg-[#74B3FF] text-primary p-1 rounded-lg leading-loose">
+            @eternalmoon1234
+          </span>
+        </a>
+        . Reach out to me at{" "}
+        <span className="text-[#74B3FF] underline">
+          gautamparanjape8@gmail.com
+        </span>
+      </p>
     </section>
   );
 };
--- utils/links.ts
+++ utils/links.ts
@@ -1,5 +1,10 @@
+// Contacts
 export const linkedinUrl: string = "https://linkedin.com/in/gparanjape";
 export const githubUrl: string = "https://github.com/eternalmoon1234";
 export const youtubeUrl: string =
   "https://www.youtube.com/channel/UCylERzQ4X5gm58R-erHdmBg";
 export const mailUrl: string = "https://google.com";
+
+// Other
+export const foyerLink: string = "https://foyer.work"
+export const tripleyLink: string = "https://tripley.app"
