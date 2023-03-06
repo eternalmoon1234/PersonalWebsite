077f4a7b0edcd1a1ab8e7f82f9796d9540c1a04d
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2023-03-06 14:03:19 -0800

    refactor: updated sizes

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import { ProjectEntity as ProjectCardProps } from "@/entities/ProjectEntity";
+import { AiOutlineLink as LinkButton } from "react-icons/ai";
 import { FC } from "react";
 
 export const ProjectCard: FC<ProjectCardProps> = ({
@@ -11,11 +12,14 @@ export const ProjectCard: FC<ProjectCardProps> = ({
     <div className="w-full bg-[#3166A5] rounded-lg shadow">
       <img className="p-4" src={imageUrl} alt={`${title} image`} />
       <div className="px-5 pb-5 space-y-4">
+        <div className="flex justify-start items-center space-x-2">
         <h5 className="text-xl font-beatriceMedium text-white">{title}</h5>
+        <LinkButton color="white" size={20} />
+        </div>
 
-        <p className="font-beatrice text-white">{description}</p>
+        <p className="font-beatrice text-white text-sm">{description}</p>
 
-        <p className="text-white">{skills}</p>
+        <p className="text-white text-sm">{skills}</p>
       </div>
     </div>
   );
