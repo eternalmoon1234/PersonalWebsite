b7ef9d0a00e3d305bb6cf7bd5e5434d8d6b3e0ce
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-03-08 12:12:39 -0800

    refactor: created data.ts

---
+++ data/data.ts
@@ -0,0 +1,65 @@
+import { ExperienceEntity } from "@/entities/ExperienceEntity";
+import { ProjectEntity } from "@/entities/ProjectEntity";
+import { foyerUrl, neusleepUrl, tripleyUrl } from "@/utils/constants";
+
+export const projects: ProjectEntity[] = [
+    {
+      title: "Tripley",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      skills: "React, TypeScript, TailwindCSS",
+      viewLink: "https://google.com",
+      githubLink: "https://github.com",
+      imageUrl:
+        "https://media.licdn.com/dms/image/C562DAQFbz0Fhxv0mSw/profile-treasury-image-shrink_800_800/0/1672799338241?e=1673496000&v=beta&t=jELjtV5W1v-ZEuhUGVOFPNR8Cc36Iyrtvryh8hS3d6M",
+    },
+    {
+      title: "Project 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      skills: "React, TypeScript, TailwindCSS",
+      viewLink: "https://google.com",
+      githubLink: "https://github.com",
+      imageUrl: "/project2.jpg",
+    },
+    {
+      title: "Project 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      skills: "React, TypeScript, TailwindCSS",
+      viewLink: "https://google.com",
+      githubLink: "https://github.com",
+      imageUrl: "/project3.jpg",
+    },
+  ];
+
+export  const experiences: ExperienceEntity[] = [
+    {
+      title: "Product Engineer",
+      organizationColor: "foyerPurple",
+      organization: "Foyer",
+      organizationUrl: foyerUrl,
+      description:
+        "Seeing the Merdaugh trial on the news today perked my interest in the case so I watched this DocuSeries tonight…Wow!",
+      skills: "Big Data · Firebase · Node.js · React.js · TypeScript",
+      dateRange: "Jan 2023 - Present",
+    },
+    {
+      title: "Co-Founder",
+      organizationColor: "tripleyBlue",
+      organization: "Tripley",
+      organizationUrl: tripleyUrl,
+      description:
+        "Building the all-in-one platform to plan and manage your trips, effectively.",
+      skills: "TS • Node",
+      dateRange: "May 2022 - Present",
+    },
+    {
+      title: "Software Engineer",
+      organizationColor: "white",
+      organization: "neusleep",
+      organizationUrl: neusleepUrl,
+      description:
+        "Developed a custom, high-performing internal developer dashboard to assist with clinical studies using the neusleep hardware device.",
+      skills:
+        "Big Data • Data Analysis • Firebase • Node.js • React.js • TypeScript",
+      dateRange: "Jun 2022 - Oct 2022",
+    },
+  ];
