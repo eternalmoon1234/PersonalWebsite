a58fd5149cf3bbceff5970441673f0a56dd46e41
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-03-11 20:51:51 -0800

    refactor: add hover effects for navbar links

--- components/navbar/NavbarLink.tsx
+++ components/navbar/NavbarLink.tsx
@@ -9,8 +9,9 @@ export const NavbarLink: FC<NavbarLinkProps> = ({ title, onClick }) => {
   return (
     <span
       onClick={onClick}
-      className="hover:cursor-pointer text-xl text-primary-light font-syne"
+      className="hover:cursor-pointer text-xl text-primary-light font-syne relative group"
     >
+      <span className="absolute -bottom-1 left-0 w-0 h-1 rounded-lg bg-blue-400 transition-all group-hover:w-full"></span>
       ./{title}
     </span>
   );
