25b6644e4a4df2ae46d5dc052e76a5e61e72489d
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-03-14 19:07:20 -0700

    feat: finished responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <footer className="rounded-lg shadow mt-12">
+    <footer className="rounded-lg shadow lg:mt-12">
       <hr className=" border-gray-700 sm:mx-auto w-full" />
       <span className="flex justify-center items-center text-center font-beatrice text-sm text-gray-200 sm:text-left py-5">
         © {new Date().getFullYear()} Gautam Paranjape. All rights reserved.
