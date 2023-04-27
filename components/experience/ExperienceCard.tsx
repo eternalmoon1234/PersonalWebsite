69cf49a81218bd61bc4a2c36954826a55649ed54
eternalmoon1234 <gautamparanjape8@gmail.com>
2023-04-27 13:32:35 -0700

    refactor: cleaned up navbar

--- components/experience/ExperienceCard.tsx
+++ components/experience/ExperienceCard.tsx
@@ -1,5 +1,5 @@
 import { FC, Fragment } from "react";
-import { AiOutlineLink as LinkButton } from "react-icons/ai";
+import { BiLinkExternal as LinkButton } from "react-icons/bi";
 import { ExperienceEntity } from "@/entities/ExperienceEntity";
 import { Spacer } from "@/components/Spacer";
 
@@ -26,14 +26,12 @@ export const ExperienceCard: FC<ExperienceEntity> = ({
           <a href={organizationUrl} target="_blank">
             <span className={`text-${organizationColor} font-beatriceSemibold`}>
               {organization}
-            </span>
-            <a href={""} target="_blank">
               <LinkButton
                 color="white"
                 size={20}
                 className="hover:cursor-pointer"
               />
-            </a>
+            </span>
           </a>
         </h6>
         <p className="text-sm lg:hidden">{dateRange}</p>
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { FC } from "react";
 import {
-  AiFillLinkedin as LinkedinButton,
-  AiFillYoutube as YoutubeButton,
-  AiFillGithub as GithubButton,
-  AiFillMail as MailButton,
-  AiOutlineTwitter as TwitterButton,
+  AiFillLinkedin,
+  AiFillYoutube,
+  AiFillGithub,
+  AiFillMail,
+  AiOutlineTwitter
 } from "react-icons/ai";
 import { scrollTo } from "@/utils/scrollTo";
 import { NavbarLink } from "./NavbarLink";
@@ -19,6 +19,14 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({ scrollLinks }) => {
   const router = useRouter();
 
+  const handleContactClick = () => {
+    if (scrollLinks) {
+      scrollTo("contact");
+    } else {
+      router.push(`mailto:${constants.contacts.email}`);
+    }
+  };
+
   return (
     <nav className="flex items-center justify-between pt-7">
       <div className="mx-auto flex w-full max-w-7xl px-5 justify-between">
@@ -26,49 +34,55 @@ export const Navbar: FC<NavbarProps> = ({ scrollLinks }) => {
           <Link href="/">
             <h3 className="flex">
               <span className="text-primary-light font-syne">g</span>
-              <span className="hidden lg:flex lg:visible font-syne lg:pr-1">autam</span>
+              <span className="hidden lg:flex lg:visible font-syne lg:pr-1">
+                autam
+              </span>
               <span className="text-primary-light font-syne">p</span>
-              <span className="hidden lg:flex lg:visible font-syne">aranjape</span>
+              <span className="hidden lg:flex lg:visible font-syne">
+                aranjape
+              </span>
             </h3>
           </Link>
           <div className="space-x-7 hidden lg:flex lg:visible">
             <NavbarLink
-              onClick={() =>
-                scrollLinks ? scrollTo("work") : router.push("/")
-              }
+              onClick={() => {
+                if (scrollLinks) {
+                  scrollTo("work");
+                } else {
+                  router.push("/");
+                }
+              }}
               title="work"
             />
+            <NavbarLink onClick={() => router.push("/projects")} title="projects" />
             <NavbarLink
-              onClick={() => router.push("/projects")}
-              title="projects"
-            />
-            <NavbarLink
-              onClick={() =>
-                scrollLinks ? scrollTo("about") : router.push("/")
-              }
+              onClick={() => {
+                if (scrollLinks) {
+                  scrollTo("about");
+                } else {
+                  router.push("/");
+                }
+              }}
               title="about"
             />
-            <NavbarLink
-              onClick={() => router.push(`mailto:${constants.contacts.email}`)}
-              title="contact"
-            />
+            <NavbarLink onClick={handleContactClick} title="contact" />
           </div>
         </div>
         <div className="flex items-center space-x-2 sm:space-x-4">
-          <a href={constants.contacts.linkedin} target="_blank">
-            <LinkedinButton color="white" size={30} />
+          <a href={constants.contacts.linkedin} target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin color="white" size={30} />
           </a>
-          <a href={constants.contacts.github} target="_blank">
-            <GithubButton color="white" size={30} />
+          <a href={constants.contacts.github} target="_blank" rel="noopener noreferrer">
+            <AiFillGithub color="white" size={30} />
           </a>
-          <a href={constants.contacts.youtube} target="_blank">
-            <YoutubeButton color="white" size={30} />
+          <a href={constants.contacts.youtube} target="_blank" rel="noopener noreferrer">
+            <AiFillYoutube color="white" size={30} />
           </a>
-          <a href={constants.contacts.twitter} target="_blank">
-            <TwitterButton color="white" size={30} />
+          <a href={constants.contacts.twitter} target="_blank" rel="noopener noreferrer">
+            <AiOutlineTwitter color="white" size={30} />
           </a>
           <a href={`mailto:${constants.contacts.email}`}>
-            <MailButton color="white" size={30} />
+            <AiFillMail color="white" size={30} />
           </a>
         </div>
       </div>
